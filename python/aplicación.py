aaabde9c1949abe7f34004298ed5de614d46ce36
isravazquez <isradronemx@gmail.com>
2022-11-26 09:41:45 -0600

    Desarrollador 1

--- python/aplicación.py
+++ python/aplicación.py
@@ -1,5 +1,5 @@
 def main():
   # Imprimir en pantalla mensaje.
-  print("Hola Bedu! Esto Es Codigo de la Rama Principal!")
+  print("Hola Bedu! Esto Es Codigo del Desarrollador 1")
 # Llamando a funcion principal.
 main()
